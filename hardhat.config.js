b3dbcf24de40f84134452c5c5c47906a915d457d
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-15 20:19:59 +0100

    Created env variables to contain secrets.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/<put here your infura key>`,
-      accounts: [`put here your private key to the Sepolia ETH account`],
+      url: process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
