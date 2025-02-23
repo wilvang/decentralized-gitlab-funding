4844ac8c4e94b325a8722bccb50cc2e8697cfa9a
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-24 00:28:04 +0100

    Created web3 frontend to communicate with the contracts. Still some issues with deployment to sepolia.

--- ignition/modules/gitlabFundingModule.js
+++ ignition/modules/gitlabFundingModule.js
@@ -1,14 +1,17 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 require('dotenv').config();
+const { artifacts } = require("hardhat");
 
 module.exports = buildModule("gitlabFundingModule", (m) => {
-
   const fundManager = m.contract("FundManager");
-  const validatorMultiSig = m.contract("ValidatorMultiSig", { arg1: fundManager.address, arg2: process.env.VALIDATORS });
-  const developerPayouts = m.contract("DeveloperPayouts", { arg1: fundManager.address, arg2: validatorMultiSig.address });
+  const validatorMultiSig = m.contract("ValidatorMultiSig", {
+    arg1: fundManager.address,
+    arg2: process.env.VALIDATORS,
+  });
+  const developerPayouts = m.contract("DeveloperPayouts", {
+    arg1: fundManager.address,
+    arg2: validatorMultiSig.address,
+  });
 
   return { fundManager, validatorMultiSig, developerPayouts };
 });
