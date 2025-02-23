1acbd9e3fe762c6e5adcab3e310d4d4c86697d35
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-23 19:37:06 +0100

    Modified the validator inetrface to match the updated contract.

--- contracts/interfaces/IValidatorMultiSig.sol
+++ contracts/interfaces/IValidatorMultiSig.sol
@@ -10,14 +10,14 @@ pragma solidity ^0.8.28;
 interface IValidatorMultiSig {
     /**
      * @notice Approve work for a specific issue.
-     * @param issueId The ID of the issue to approve.
+     * @param mergeRequest The ID of the merge request to approve.
      */
-    function approveWork(uint256 issueId) external;
+    function approveWork(uint256 mergeRequest) external;
 
     /**
      * @notice Release funds for a specific issue to a developer.
      * @param issueId The ID of the issue to release funds for.
      * @param developer The address of the developer to receive the funds.
      */
-    function releaseFunds(uint256 issueId, address developer) external payable;
+    function releaseFunds(uint256 issueId, uint256 mergeRequest, address developer) external payable;
 }
